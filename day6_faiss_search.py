04fa74b922a1b96416a5e5e9e30b8ad09d6fe28a
litao <13368447@qq.com>
2025-07-24 15:34:30 +0800

    day6

--- day6_faiss_search.py
+++ day6_faiss_search.py
@@ -1,1 +1,15 @@
-import openai
+import numpy as np
+import matplotlib.pyplot as plt
+
+d = 512
+n_data = 2000
+np.random.seed(0)
+data = []
+mu = 3
+sigma = 0.1
+for i in range(n_data):
+    data.append(np.random.normal(mu, sigma, d))
+data = np.array(data).astype(np.float32)
+
+plt.hist(data[5])
+plt.show()
