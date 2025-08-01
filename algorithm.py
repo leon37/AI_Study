85a89e23945dae322879634cf2b6ab56e4f04677
litao <13368447@qq.com>
2025-08-01 19:32:00 +0800

    nltk ch01 half

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,6 @@
-from collections import defaultdict
-from typing import List
+import nltk
 
-
-class Solution:
-    def countMaxOrSubsets(self, nums: List[int]) -> int:
-        max_value = 0
-        subset_count = 0
-        for right in range(len(nums)):
-            cur_value = 0
-            for left in range(right+1):
-                cur_value = cur_value | nums[left]
-                if cur_value > max_value:
-                    max_value = cur_value
-                    subset_count = 1
-                elif cur_value == max_value:
-                    subset_count += 1
-        return subset_count
+nltk.download()
 
 
 
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.countMaxOrSubsets([3, 1]))
---
+++ nltk_learning/ch01.py
@@ -0,0 +1,6 @@
+from nltk.book import *
+
+# text1.concordance('monstrous')
+# text1.similar('monstrous')
+# text2.similar('monstrous')
+text1.common_contexts(['monstrous', 'very'])
